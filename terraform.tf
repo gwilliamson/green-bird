0aac152dcf232c30798b4e772ba589db2fb33bd8
gwilliamson <gregoryw@23andme.com>
2024-12-13 07:30:30 -0800

    Add working hello world

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.38.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.3"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7.0"
+    }
+  }
+
+  required_version = "~> 1.10"
+}
+
