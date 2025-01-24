d3689137c8ce535b511bbe37e6fa30accb99e249
gwilliamson <gregoryw@23andme.com>
2025-01-24 09:28:53 -0800

    Set document.title

--- modules/www/app/src/main.ts
+++ modules/www/app/src/main.ts
@@ -2,6 +2,8 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+document.title = import.meta.env.VITE_APP_NAME || 'Default App Name';
+
 const app = mount(App, {
   target: document.getElementById('app')!,
 })
